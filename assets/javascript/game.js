8efed58785f5da35cf8a5e0fc38e32365db61e0c
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-08-07 23:50:44 -0500

    initial commit and save

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var win = 0;
+    var lose = 0;
+
+    const wins = document.getElementById('wins');
+    const loss = document.getElementById('loss');
+
+    for (var i = 0; i < letters.length; i++){
+
+
+
+    }
+
+
+
+
+
+
+}
